3358ed7f214e330b8b8d42320a6728957c888ea6
Oleg <Oleg.Ivashko@yandex.ua>
2018-04-25 02:00:45 +0300

    dd

--- src/main/java/io/github/oleiva/ivasoft/controllers/HomeController.java
+++ src/main/java/io/github/oleiva/ivasoft/controllers/HomeController.java
@@ -31,7 +31,7 @@ public class HomeController {
 //    https://stackoverflow.com/questions/12416738/how-to-use-herokus-ephemeral-filesystem?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 //    private static String UPLOADED_FOLDER = "/home/ivasoft/Documents/Owner/spring-boot-heroku/Cashe/";
 //    private static String UPLOADED_FOLDER = "d:\\config\\";
-    private static String UPLOADED_FOLDER = "/tmp";
+    private static String UPLOADED_FOLDER = "/tmp/";
 
     @Autowired
     public HomeController(RecordRepository repository) {
